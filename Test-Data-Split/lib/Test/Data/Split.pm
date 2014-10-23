7bc424e9e9da971d1aebb398f944e426654e1491
Shlomi Fish <shlomif@shlomifish.org>
2014-10-23 21:09:35 +0300

    Add the skeleton of Test-Data-Split.

---
+++ Test-Data-Split/lib/Test/Data/Split.pm
@@ -0,0 +1,15 @@
+package Test::Data::Split;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.0.1';
+
+=head1 NAME
+
+Test::Data::Split - split data-driven test into several test scripts.
+
+=cut
+
+1;
+
